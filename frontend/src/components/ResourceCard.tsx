2fd34ebc2555d9ea66592746b4a36802270776a0
Austin Raney <araney@cuahsi.org>
2022-08-16 16:18:56 -0400

    add resource card component

---
+++ frontend/src/components/ResourceCard.tsx
@@ -0,0 +1,30 @@
+import Card, { CardProps } from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { SearchResourceResult } from "../graphql/queries";
+import { formatBytes } from "../utilities/humanReadableByteSize"
+
+export interface Props {
+    props: SearchResourceResult
+    cardProps?: CardProps
+}
+
+export default function SearchItem({ props, cardProps }: Props) {
+    const { title, creators, created, nFiles, size } = props
+
+    return (
+        <Card {...cardProps}>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography gutterBottom variant="body1" component="div">
+                    {creators?.map(value => value?.name).join(", ")}
+                </Typography>
+                <Typography variant="body2">
+                    Uploaded: {created?.slice(0, 10)} | {nFiles} files(size: {formatBytes(size, 2)})
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
