c33b618d281e8ac85b3cd176757d12dfb2b6bcbe
Austin Raney <araney@cuahsi.org>
2022-08-16 13:19:02 -0400

    export toolbar height as a constant

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -3,17 +3,17 @@ import MenuBar from "./MenuBar"
 import MuiAppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 
-const toolbar_height = "64px"
+export const TOOLBAR_HEIGHT = "64px"
 
 export const AppBar = () => {
     return (
         <>
-            <MenuBar height={toolbar_height} />
             <MuiAppBar component="nav" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                <Toolbar sx={{ height: toolbar_height }}>
+                <Toolbar sx={{ height: TOOLBAR_HEIGHT }}>
                     <img src="/iguide_logo_15_percent.png" alt="I-Guide Logo" style={{ height: "inherit", padding: "8px" }} />
                 </Toolbar>
             </MuiAppBar>
+            <MenuBar height={TOOLBAR_HEIGHT} />
         </>
     );
 }
