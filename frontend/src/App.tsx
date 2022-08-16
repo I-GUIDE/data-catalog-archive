4b18bf7e3b7d4456a1148bc20baa996f893727d6
Austin Raney <araney@cuahsi.org>
2022-08-16 12:21:44 -0400

    setup react router dom in App. still needs to be connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,50 @@
-import React from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import MenuBar from "./components/MenuBar"
-import Datasets from "./pages/Datasets"
-import Discover from "./pages/Discover"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AppBar from './components/AppBar';
+import Datasets from "./pages/Datasets";
+import Discover from "./pages/Discover";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#184848"
+    }
+  }
+});
+
 
 function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>Them
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
 
-      <MenuBar />
+          <AppBar />
 
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', justifyContent: 'center', flexDirection: 'row' }}
-      >
-        <Box sx={{ maxWidth: '1200px' }}>
-          <Toolbar />
-          <Discover />
-          <Datasets />
-        </Box>
-      </Box>
-    </Box>
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, display: 'flex', justifyContent: 'center', flexDirection: 'row' }}
+          >
+            <Box sx={{ maxWidth: '1200px' }}>
+              <br />
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="search" element={<Search />} />
+                <Route path="datasets" element={<Discover />} />
+                <Route path="notebooks" element={<Datasets />} />
+              </Routes>
+            </Box>
+          </Box>
+        </Box >
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
