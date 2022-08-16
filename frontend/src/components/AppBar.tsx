1bb934fc584424233bec524d98d7882b304480af
Austin Raney <araney@cuahsi.org>
2022-08-16 12:20:47 -0400

    embed MenuBar comp into AppBar comp. also add logo

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,1 +1,21 @@
-export default {};
+import React from 'react';
+import MenuBar from "./MenuBar"
+import MuiAppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+
+const toolbar_height = "64px"
+
+export const AppBar = () => {
+    return (
+        <>
+            <MenuBar height={toolbar_height} />
+            <MuiAppBar component="nav" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar sx={{ height: toolbar_height }}>
+                    <img src="/iguide_logo_15_percent.png" alt="I-Guide Logo" style={{ height: "inherit", padding: "8px" }} />
+                </Toolbar>
+            </MuiAppBar>
+        </>
+    );
+}
+
+export default AppBar
