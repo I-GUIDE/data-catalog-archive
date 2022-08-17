8dc669801023d4b88faddebbb52b584b14d22354
Austin Raney <araney@cuahsi.org>
2022-08-17 11:34:06 -0400

    use custom css in Discover page

--- frontend/src/pages/Discover.tsx
+++ frontend/src/pages/Discover.tsx
@@ -2,16 +2,15 @@ import ResourceCard from "../components/ResourceCard";
 import { useGetResources } from '../graphql/queries';
 
 import Container from "../components/Container";
-
-
-
+import "./Discover.css"
 
 export const Discover = () => {
     const { results } = useGetResources(24)
 
     if (results === undefined) return null
 
-    return <Container sx={{ height: "auto", display: "grid", gridTemplateColumns: "repeat(4, 1fr)" }}>
+    // css defined in Discover.css. Media queries required
+    return <Container id="discover-container">
         {
             results.map((props, idx) => {
                 return <ResourceCard props={{ ...props }} cardProps={{ style: { height: "100%" }, key: props.id }} />
