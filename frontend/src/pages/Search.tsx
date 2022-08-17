570e60e81c598f2921d68add8bb923d4cfd772bf
Austin Raney <araney@cuahsi.org>
2022-08-17 17:17:43 -0400

    rough cut of Search page. needs heavy refactorin and more thought, but its in the right direction

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MuiContainer from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
@@ -6,6 +6,10 @@ import SearchIcon from '@mui/icons-material/Search';
 import { TOOLBAR_HEIGHT } from '../components/AppBar';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
+import { useSearch } from '../graphql/queries';
+import WideSearchListing from '../components/WideSearchListing';
+import { useSearchParams, } from "react-router-dom"
+import createSlug from '../utilities/createSlug';
 
 const Container = styled(MuiContainer)(() => ({
     display: "flex",
@@ -18,7 +22,23 @@ const Container = styled(MuiContainer)(() => ({
 
 export const Search = () => {
     const [query, setQuery] = useState<string>("")
+    // number of search results returned should be a power of 4 for layout purposes
+    const { results, loading, useQuery: queryFn } = useSearch(48)
+    const [searchParams, setSearchParams] = useSearchParams()
 
+    const handleQuery = () => {
+        // empty string dont query
+        if (!query.length) return
+        const slug = createSlug(query)
+        setSearchParams({ q: slug })
+        queryFn(query)
+    }
+
+    function handleTextFieldEnterKeydown(event: React.KeyboardEvent<HTMLDivElement>) {
+        if (event.code === "Enter") {
+            handleQuery()
+        }
+    }
 
     return <Container>
         <TextField
@@ -26,6 +46,7 @@ export const Search = () => {
             placeholder="aging dams"
             onChange={q => setQuery(q.target.value)
             }
+            onKeyDown={handleTextFieldEnterKeydown}
             sx={{
                 marginTop: `calc(20vh - ${TOOLBAR_HEIGHT})`,
                 minWidth: "100px",
@@ -42,7 +63,8 @@ export const Search = () => {
             }}
         >
         </TextField>
-        <Button variant="outlined">Search</Button>
+        <Button variant="outlined" onClick={handleQuery}>Search</Button>
+        {results && <WideSearchListing results={results} />}
     </Container>;
 }
 export default Search
