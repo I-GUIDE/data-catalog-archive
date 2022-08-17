ec3c5c435e3386a9cde026365392b617a8791d0c
Austin Raney <araney@cuahsi.org>
2022-08-17 13:56:33 -0400

    remove erroneous logging

--- frontend/src/components/SearchItem.tsx
+++ frontend/src/components/SearchItem.tsx
@@ -31,7 +31,6 @@ const maxDescriptionLength = 250
 export default function SearchItem({ props, cardProps }: Props) {
     // const { title, description, authors, nFiles, uploadDate, size } = props
     const { title, creators, abstract, created, nFiles, size } = props
-    console.log(props)
 
     return (
         <Card {...cardProps}>
@@ -52,30 +51,3 @@ export default function SearchItem({ props, cardProps }: Props) {
         </Card>
     )
 }
-
-
-// export default function MediaCard() {
-//     return (
-//         <Card sx={{ maxWidth: 345 }}>
-//             <CardMedia
-//                 component="img"
-//                 height="140"
-//                 image="/static/images/cards/contemplative-reptile.jpg"
-//                 alt="green iguana"
-//             />
-//             <CardContent>
-//                 <Typography gutterBottom variant="h5" component="div">
-//                     Lizard
-//                 </Typography>
-//                 <Typography variant="body2" color="text.secondary">
-//                     Lizards are a widespread group of squamate reptiles, with over 6,000
-//                     species, ranging across all continents except Antarctica
-//                 </Typography>
-//             </CardContent>
-//             <CardActions>
-//                 <Button size="small">Share</Button>
-//                 <Button size="small">Learn More</Button>
-//             </CardActions>
-//         </Card>
-//     );
-// }
