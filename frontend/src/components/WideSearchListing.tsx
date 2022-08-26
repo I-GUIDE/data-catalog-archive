a55baa9537c35a68f9bb4375ca720c61e9434f39
Austin Raney <araney@cuahsi.org>
2022-08-26 12:53:23 -0400

    wide search listing pulse a skeleton view while they dont have data

--- frontend/src/components/WideSearchListing.tsx
+++ frontend/src/components/WideSearchListing.tsx
@@ -3,6 +3,7 @@ import ListItem from '@mui/material/ListItem';
 import { SearchResourcesResults } from "../graphql/queries";
 import { Link } from "react-router-dom"
 import SearchItem from "./SearchItem";
+import SearchItemSkeleton from './SearchItemSkeleton';
 
 export interface Props {
     results: SearchResourcesResults
@@ -10,17 +11,26 @@ export interface Props {
 
 export function WideSearchListing({ results }: Props) {
 
-    return <List>
+    return <List style={{ width: "100%" }}>
         {
-            results?.map((value, idx) => {
-                return <ListItem key={idx.toString()}>
-                    {/* id will change once we switch to new db collection */}
-                    <Link to={`../holdings/${value.id?.slice(value.id.length - 32)}`} state={value} style={{ textDecoration: 'none' }}>
-                        <SearchItem cardProps={{ sx: { width: "100%" } }} props={{ ...value }} />
-                    </Link>
-                </ListItem>
-            })
+            !results ?
+                [...Array(10)].map(i => {
+                    return <ListItem key={i}>
+                        <SearchItemSkeleton animation="pulse" cardProps={{ style: { width: "100%" } }} />
+                    </ListItem>
+                })
+                :
+                results?.map((value, idx) => {
+                    return <ListItem key={idx.toString()}>
+                        {/* id will change once we switch to new db collection */}
+                        <Link to={`../holdings/${value.id?.slice(value.id.length - 32)}`} state={value} style={{ textDecoration: 'none', width: "100%" }}>
+                            <SearchItem cardProps={{ sx: { width: "100%" } }} props={{ ...value }} />
+                        </Link>
+                    </ListItem>
+                })
         }
+
+
     </List>
 
 }
