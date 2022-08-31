5cf4ae705a3af5a0b8472778bc907d979f71f7ef
Austin Raney <araney@cuahsi.org>
2022-08-31 13:54:05 -0400

    update App imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,21 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppBar, { TOOLBAR_HEIGHT } from './components/AppBar';
+import AppBar, { TOOLBAR_HEIGHT } from "./components/AppBar";
 import Datasets from "./pages/Datasets";
 import Holding from "./pages/Holding";
 import Home from "./pages/Home";
-import Notebooks from './pages/Notebooks';
+import Notebooks from "./pages/Notebooks";
 import Search from "./pages/Search";
-
-
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#184848"
-    }
-  }
+      main: "#184848",
+    },
+  },
 });
 
 const Main = styled(Box)(({ theme }) => ({
@@ -25,15 +23,14 @@ const Main = styled(Box)(({ theme }) => ({
   flexGrow: 1,
   background: theme.palette.background.default,
   padding: "0em 1em",
-  marginTop: TOOLBAR_HEIGHT
+  marginTop: TOOLBAR_HEIGHT,
 }));
-
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: "flex" }}>
           <CssBaseline />
 
           <AppBar />
@@ -54,7 +51,7 @@ function App() {
           </Main>
         </Box>
       </ThemeProvider>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
 
