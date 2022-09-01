5fc8f8bd5270ce247619e91d912c73c259907929
Austin Raney <araney@cuahsi.org>
2022-09-01 17:07:06 -0400

    update getUrl util

--- frontend/src/utilities/getUrl.ts
+++ frontend/src/utilities/getUrl.ts
@@ -1,6 +1,8 @@
 const PUBLIC_URL = process.env.PUBLIC_URL;
 
 export function getUrl(path: string): string {
+    if (path === "/" && PUBLIC_URL === "") return path
+    if (path === "/") return PUBLIC_URL
     return `${PUBLIC_URL}/${path}`;
 }
 
