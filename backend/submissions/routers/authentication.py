7c942df6a2a9a7e9a7ef8496855c6abfb71d8b99
Scott Black <sblack@cuahsi.org>
2023-05-10 11:04:22 -0600

    roll our own authentication

--- backend/submissions/routers/authentication.py
+++ backend/submissions/routers/authentication.py
@@ -2,19 +2,24 @@ from fastapi import APIRouter, Request
 from starlette.responses import RedirectResponse
 import uvicorn
 
-from fastapi_oidc_auth.auth import OpenIDConnect
+from submissions.fastapi_oidc_auth.auth import OpenIDConnect
 
 host = "https://auth.cuahsi.io"
-realm = "IGUIDE"
-client_id = "iguide-catalog"
-client_secret = "jC8NwfW8KHMv0ww0WEFvpYSv9R5ddjAD"
-app_uri = "http://localhost/api/"
+realm = "HydroShare"
+client_id = "local_iguide_api"
+client_secret = "AyPQBiRP1FAJ7bU8rIUopgtFExc6ySkR"
+app_uri = "https://localhost/api/"
+redirect_uri = "https://localhost/api/"
+
+#host = "http://keycloak:8080/auth"
+#realm = "master"
+#client_id = "test-client"
+#client_secret = "jC8NwfW8KHMv0ww0WEFvpYSv9R5ddjAD"
+#app_uri = "https://localhost/api/"
+#redirect_uri = "https://localhost/api"
 
 
-class CustomOpenIDConnect(OpenIDConnect):
-    well_known_pattern = "{}/realms/{}/.well-known/openid-configuration"
-
-oidc = CustomOpenIDConnect(host, realm, app_uri, client_id, client_secret)
+oidc = OpenIDConnect(host, realm, app_uri, client_id, client_secret, redirect_uri)
 router = APIRouter()
 
 
